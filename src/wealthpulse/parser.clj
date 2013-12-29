4aa9b9065af5f4d5471b637190208ef6ab85636b
Mark Williams <mark@readysetmark.com>
2013-12-29 17:34:26 -0600

    Hiding some useless tokens from the parse tree. Started writing transaction transformation code.

--- src/wealthpulse/parser.clj
+++ src/wealthpulse/parser.clj
@@ -1,5 +1,21 @@
 (ns wealthpulse.parser
-  (:require [instaparse.core :as insta]))
+	(:require [instaparse.core :as insta]
+		[clojure.string :as string]))
+
+
+; Should use java.util.Date instead of java.text.SimpleDateFormat?
+
+(defn transform-transaction
+	"Transform a parsed transaction into a more usable form."
+	[transaction]
+	(insta/transform 
+		{:date (fn [x] [:date (.parse (java.text.SimpleDateFormat. "yyyy/MM/dd") x)])
+		 :status (fn [x] [:status (if (= x \!) :uncleared :cleared)])
+		 :code (fn [x] [:code (string/trim x)])
+		 :payee (fn [x] [:payee (string/trim x)])
+		 :note (fn [x] [:note (string/trim x)])
+		 :quantity (fn [x] [:quantity (bigdec (string/replace x "," ""))])}
+		transaction))
 
 
 (def parse-transaction
@@ -14,3 +30,4 @@
 		  extract-transaction-regex #"(?m)^\d.+(?:\r\n|\r|\n)(?:[ \t]+.+(?:\r\n|\r|\n))+"
 		  transaction-strings (re-seq extract-transaction-regex file-contents)]
 		(map parse-transaction transaction-strings)))
+
