d9cc7b0c47c248398818f7501458b93c59d7fcd2
Mark Williams <mark@readysetmark.com>
2014-01-21 21:04:10 -0600

    Started work on transforming transaction parse tree into entry list

--- src/wealthpulse/parser.clj
+++ src/wealthpulse/parser.clj
@@ -2,6 +2,28 @@
 	(:require [instaparse.core :as insta]
 		[clojure.string :as string]))
 
+; Journal records
+(defrecord Header [date status code payee note])
+
+(defn process-header
+	"Transform a header from the parse tree into a Header record."
+	[[_ & values]]
+	(let [collect-values
+			(fn [coll [key value]] 
+				(cond
+					(= key :date) (assoc coll :date (.parse (java.text.SimpleDateFormat. "yyyy/MM/dd") value))
+					(= key :status) (assoc coll :status (if (= value \!) :uncleared :cleared))
+					(= key :code) (assoc coll :code (string/trim value))
+					(= key :payee) (assoc coll :payee (string/trim value))
+					(= key :note) (assoc coll :note (string/trim value))))]
+		(reduce collect-values {} values)))
+
+(defn process-transaction
+	"Transform a parse-tree transaction into entries."
+	[[_ header entries]]
+	(process-header header))
+	;(doall (map println [header entries])))
+
 
 (defn transform-transaction
 	"Transform a parsed transaction into a more usable form."
@@ -29,4 +51,3 @@
 		  extract-transaction-regex #"(?m)^\d.+(?:\r\n|\r|\n)(?:[ \t]+.+(?:\r\n|\r|\n))+"
 		  transaction-strings (re-seq extract-transaction-regex file-contents)]
 		(map parse-transaction transaction-strings)))
-
