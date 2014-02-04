193ec48d91e018b6923bd95f288093e2c33e4740
Mark Williams <mark@readysetmark.com>
2014-02-04 12:44:03 -0600

    Got very basic, static balance sheet working.

--- src/wealthpulse/web.clj
+++ src/wealthpulse/web.clj
@@ -3,33 +3,51 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.middleware.json :as json]
-            [ring.util.response :as response]))
+            [ring.util.response :as response]
+            [wealthpulse.parser :as parser]
+            [wealthpulse.query :as query])
+  (:import [java.text NumberFormat]))
+
+
+; TODO: this is here temporarily and should be moved somewhere more appropriate
+(defn annotate-balances
+  [[balances total]]
+  (let [format-balance #(.format (NumberFormat/getCurrencyInstance) %)
+        sorted-balances (vec (map (fn [[account balance]] {:account account :balance (format-balance balance)}) (sort balances)))]
+    (conj sorted-balances {:account "Total" :balance (format-balance total)})))
+
 
 
 (defn api-routes
   "Define API routes."
-  [state]
+  [journal]
   (routes
-    (GET "/nav" [] (response/response {:listening-to state}))
-    (GET "/balance" [] "...")
+    (GET "/nav" [] (response/response
+                      [{:title "Balance Sheet" :url "#/balance?parameters=assets liabilities :exclude units :title Balance Sheet"}
+                       {:title "Net Worth" :url "#/networth"}
+                       {:title "Income journalment - Current Month" :url "#/balance?parameters=income expenses :period this month :title Income Statement"}
+                       {:title "Income Statement - Previous Month" :url "#/balance?parameters=income expenses :period last month :title Income Statement"}]))
+    (GET "/balance" [] (response/response
+                          {:title "Balance Sheet"
+                           :subtitle "As of today"
+                           :balances (annotate-balances (query/balance journal {:accounts-with ["assets" "liabilities"] :exclude-accounts-with ["units"]}))}))
     (GET "/networth" [] "...")))
 
 
 (defn app-routes
   "Define application routes."
-	[state]
+	[journal]
 	(routes
-    (context "/api" [] (-> (handler/api (api-routes state))
+    (context "/api" [] (-> (handler/api (api-routes journal))
                            (json/wrap-json-body)
                            (json/wrap-json-response)))
 		(handler/site
       (routes
-        ;(GET "/" [] (str "<h1>hello " state "</h1>"))
-        ;(GET "/" [] (response/redirect "/index.html"))
         (GET "/" [] (response/resource-response "index.html" {:root "public"}))
         (route/resources "/")
         (route/not-found "Not Found")))))
 
 
 (def handler
-  (app-routes "Pillows"))
+  (let [ledger-file (.get (System/getenv) "LEDGER_FILE")]
+    (app-routes (parser/parse-journal ledger-file))))
