9794349a6acc8178a82cc8032d2850e486c4cd24
Mark Williams <mark@readysetmark.com>
2014-02-05 14:21:00 -0600

    Updated annotate-balance to format/annotate balance data for display.

--- src/wealthpulse/web.clj
+++ src/wealthpulse/web.clj
@@ -1,6 +1,7 @@
 (ns wealthpulse.web
   (:use compojure.core)
-  (:require [compojure.handler :as handler]
+  (:require [clojure.string :as string]
+            [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.middleware.json :as json]
             [ring.util.response :as response]
@@ -12,9 +13,34 @@
 ; TODO: this is here temporarily and should be moved somewhere more appropriate
 (defn annotate-balances
   [[balances total]]
-  (let [format-balance #(.format (NumberFormat/getCurrencyInstance) %)
-        sorted-balances (vec (map (fn [[account balance]] {:account account :balance (format-balance balance)}) (sort balances)))]
-    (conj sorted-balances {:account "Total" :balance (format-balance total)})))
+  (let [padding-left-base 8
+        indent-padding 20
+        sorted-balances (sort-by first balances)
+        get-account-display (fn [account]
+                              (let [[parentage indent] (reduce (fn [[parentage indent] [other-account _]]
+                                                               (if (and (.startsWith account other-account)
+                                                                        (not= account other-account)
+                                                                        (= (.charAt account (.length other-account)) \:))
+                                                                   [other-account (inc indent)]
+                                                                   [parentage indent]))
+                                                             ["" 0]
+                                                             sorted-balances)
+                                    account-display (if (not (.isEmpty parentage))
+                                                        (.substring account (inc (.length parentage)))
+                                                        account)]
+                                [account-display indent]))
+        format-balance #(.format (NumberFormat/getCurrencyInstance) %)
+        annotated-balances (vec (map (fn [[account balance]]
+                                       (let [[account-display indent] (get-account-display account)
+                                             padding (+ padding-left-base (* indent indent-padding))]
+                                         {:account account-display
+                                          :balance (format-balance balance)
+                                          :balanceclass (string/lower-case (first (string/split account #":")))
+                                          :accountstyle (str "padding-left: " padding "px;")}))
+                                     sorted-balances))]
+    (conj annotated-balances {:account ""
+                              :balance (format-balance total)
+                              :rowclass "grand_total"})))
 
 
 
@@ -25,7 +51,7 @@
     (GET "/nav" [] (response/response
                       [{:title "Balance Sheet" :url "#/balance?parameters=assets liabilities :exclude units :title Balance Sheet"}
                        {:title "Net Worth" :url "#/networth"}
-                       {:title "Income journalment - Current Month" :url "#/balance?parameters=income expenses :period this month :title Income Statement"}
+                       {:title "Income Statement - Current Month" :url "#/balance?parameters=income expenses :period this month :title Income Statement"}
                        {:title "Income Statement - Previous Month" :url "#/balance?parameters=income expenses :period last month :title Income Statement"}]))
     (GET "/balance" [] (response/response
                           {:title "Balance Sheet"
